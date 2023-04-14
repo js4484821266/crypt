90a989396f98406d8c65f0f0e67e1e11e9dc26ab
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-14 21:39:35 +0900

    add buttons to open files

--- tktest.py
+++ tktest.py
@@ -1,5 +1,19 @@
-import tkinter,Crypto
-window=tkinter.Tk()
+import tkinter as tk
+import tkinter.filedialog as fd
+from Crypto.Cipher import AES as aes
+window = tk.Tk()
 window.title("CRYPT")
+window.geometry("500x500")
 
+for fn in ['Target', 'Key']:
+    frame = tk.LabelFrame(window, text=fn)
+    frame.pack(padx=10, fill="x")
+    button = tk.Button(frame, text="Open",
+                       command=fd.askopenfilename)
+    button.pack(side="right", padx=5, pady=5)
+    label = tk.Label(frame, 
+                     text="1234567890-=qwertyuiopasdfghjklzxcvbnm")
+    label.pack(side="left", padx=5)
+tk.Button(window, text='Quit', command=exit).pack(
+    padx=10,pady=10,side='bottom')
 window.mainloop()
