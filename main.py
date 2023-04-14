0612c6f13b62a389ca47253ccc63e9e6f46dda32
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-15 00:38:46 +0900

    print purposes of files

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-import tkinter as tk
+import itertools
+modes=[]
 import tkinter.filedialog as fd
-import tkinter.ttk as ttk
-from Crypto.Cipher import AES as aes
+import tkinter as tk
+print('CLOSE THIS WINDOW AND THE PROGRAM WILL SHUT DOWN!!!')
 window = tk.Tk()
 window.title("CRYPT")
-window.geometry("500x500")
 
-for fn in ['Target', 'Key']:
-    frame = tk.LabelFrame(window, text=fn)
-    frame.pack(padx=10, fill="x")
-    button = tk.Button(frame, text="Open",
-                       command=fd.askopenfilename)
-    button.pack(side="right", padx=5, pady=5)
-    label = tk.Label(frame, wraplength=ttk.Sizegrip.winfo_reqwidth(frame),
-                     text="1234567890-=qwertyuiopasdfghjklzxcvbnm")
-    label.pack(side="left", padx=5)
+fh = tk.Frame(window)
+fh.pack(side="top", fill="both", expand=True)
+for r,p in enumerate(['File','Key']):
+    tk.Label(fh, text=p).grid(
+        row=r, column=0, ipadx=5, ipady=5)
+
+
+def get_path():fd.askopenfilename()
+
+
+for i in range(2):
+    pass
+# for fo in files:
+#     button = tk.Button(fo, text="Open",
+#                        command=fd.askopenfilename)
+#     button.pack(side="right", padx=5, pady=5)
+#     label = tk.Label(fo,justify="left",anchor='w',
+#                      text="1234567890-=qwertyuiopasdfghjklzxcvbnm")
+#     label.pack(side="left", padx=5,fill='both',expand=True)
+
 window.mainloop()
